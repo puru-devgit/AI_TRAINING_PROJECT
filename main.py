5ee948855f5a1b8ffa2a40dd449b059dae294367
aditi3089 <aditijsr3089@gmail.com>
2026-03-27 12:50:39 +0530

    Completed till day 3

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="supply_chain",
+    user="postgres",
+    password="Puru@2107"
+)
+
+cursor = conn.cursor()
+
 app = FastAPI()
 
 @app.get("/")
@@ -9,3 +20,37 @@ def home():
 @app.get("/test")
 def test():
     return {"status": "working"}
+
+@app.get("/inventory")
+def get_inventory():
+    cursor.execute("SELECT * FROM inventory")
+    rows = cursor.fetchall()
+
+    result = []
+    for row in rows:
+        result.append({
+            "id": row[0],
+            "product_name": row[1],
+            "quantity": row[2]
+        })
+
+    return {"inventory": result}
+
+@app.get("/forecast")
+def get_forecast():
+    forecast = [100, 120, 140, 130]
+    return {"forecast": forecast}
+
+@app.get("/forecast/{product_id}")
+def forecast_product(product_id: int):
+    cursor.execute("SELECT * FROM inventory WHERE id=%s", (product_id,))
+    product = cursor.fetchone()
+
+    return {
+        "product": {
+            "id": product[0],
+            "name": product[1],
+            "current_stock": product[2]
+        },
+        "forecast": [60, 70, 80]
+    }
