328737e82e72c2c45de03c33dd831a8b56c33301
Trisha Nandi <trishanandy285@gmail.com>
2026-03-27 12:50:30 +0530

    added till day 4

--- weather_risk.py
+++ weather_risk.py
@@ -2,43 +2,46 @@ import requests
 
 API_KEY = "b3eae4351f337912a4b9882a91835ade"
 
+risk_data = {
+    "Rain": "Heavy rain can delay transportation and delivery of goods.",
+    "Thunderstorm": "Thunderstorms may disrupt logistics and supply routes.",
+    "Clouds": "Minimal impact on transportation.",
+    "Clear": "No major supply chain risk."
+}
 
-# Day 1 → Fetch weather data
+# Day 1
 def get_weather(city):
-
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
-
     response = requests.get(url)
     data = response.json()
+    return data["weather"][0]["main"]
 
-    weather = data["weather"][0]["main"]
-    temp = data["main"]["temp"]
+# Day 3
+def retrieve_risk(weather):
+    if weather in risk_data:
+        return risk_data[weather]
+    else:
+        return "No risk information available"
 
-    return weather, temp
+#  Day 4
+def supply_decision(risk):
 
+    if "delay" in risk.lower():
+        return "Reorder inventory earlier"
 
-# Day 2 → Risk detection
-def check_risk(city):
-
-    weather, temp = get_weather(city)
-
-    if weather == "Rain":
-        return "Delivery delay possible"
-
-    elif weather == "Thunderstorm":
-        return "High supply chain risk"
+    elif "disrupt" in risk.lower():
+        return "Increase safety stock"
 
     else:
-        return "No major risk"
+        return "Normal supply chain operation"
 
 
-# Run the program
 city = "Bhubaneswar"
 
-weather, temp = get_weather(city)
-risk = check_risk(city)
+weather = get_weather(city)
+risk = retrieve_risk(weather)
+decision = supply_decision(risk)
 
-print("City:", city)
 print("Weather:", weather)
-print("Temperature:", temp)
-print("Risk Alert:", risk)
+print("Risk:", risk)
+print("Decision:", decision)
