e43e73797849011cc8527899ce494437659fde71
Roshni Singh <singhroshni3211@gmail.com>
2026-03-27 12:46:14 +0530

    Add files via upload

---
+++ frontend/__init__.py
@@ -0,0 +1,14 @@
+# utils/__init__.py
+from .helpers import (
+    DESIGN_CSS,
+    PLOTLY_THEME,
+    PRODUCTS,
+    RISK_INCIDENTS,
+    API_BASE,
+    backend_alive,
+    api_inventory,
+    api_weather,
+    api_history,
+    api_purchase_orders,
+    api_agent_query,
+)
